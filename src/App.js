cbea053c7e7b5fa6c818006110fca2b1b6cc6721
Ahsan hyder <39864885+ahsanhyder625@users.noreply.github.com>
2020-11-01 13:25:40 +0530

    practice1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// import './App.css';
+import './index.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	let date = new Date();
+  date = date.getHours();
+  
+  let greeting = '';
+  
+  const Styling={};
+  
+	if (date >= 1 && date < 12) {
+    greeting = 'Good Morning';
+    Styling.color='green';
+	} else if (date >= 12 && date < 19) {
+    greeting = 'Good Evening';
+    Styling.color='orange';
+	} else {
+    greeting = 'Good night';
+    Styling.color='black';
+  }
+  
+	return (
+		<div className="App">
+      <div className="box">
+      <h1 className="heading">Hello Sir, <span style={Styling}>{greeting}</span></h1>
+      </div>
+			
+		</div>
+	);
 }
 
 export default App;
